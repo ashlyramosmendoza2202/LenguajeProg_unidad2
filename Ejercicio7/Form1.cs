0403137eab1582e2da0285fe3bdf8429b970cb2c
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 18:07:13 -0400

    cargando

---
+++ Ejercicio7/Form1.cs
@@ -0,0 +1,34 @@
+﻿namespace Ejercicio7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        {
+            int numero;
+
+            if (int.TryParse(textBox1.Text, out numero))
+            {
+                MessageBox.Show("✅ Es un número válido",
+                                "Correcto",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("❌ Error: Debes ingresar un número entero",
+                                "Entrada inválida",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
+}
