d548b1e3287d09997276dd1f1992f37d02bd0b68
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 18:11:47 -0400

    cargando

---
+++ Actividad8/Form1.cs
@@ -0,0 +1,18 @@
+namespace Actividad8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, soy Ashly Ramos - LA-2025-00177",
+                "Saludo",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
